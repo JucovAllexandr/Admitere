af222b5562e8dfee5cfa4dd50f1039fa44c40d84
sania <san_zhuk@mail.ru>
2016-06-07 12:33:51 +0300

    test

--- resources/views/pdf/admiterea.blade.php
+++ resources/views/pdf/admiterea.blade.php
@@ -26,8 +26,24 @@
 <p>Adresa: r.{{$e->raion}}.,</p>
 <p>Codul personal: {{$e->cod_personal}}</p>
 <p>Seria: </p>
-<p>Seria: </p>
-<p>Seria: </p>
+<p>Nr: </p>
+<p>Eliberat de oficiu: </p>
+<p>Data: </p>
+<p>Nationalitatea: </p>
+{{--                        --}}
+<p>Institutia de invatamint absolvita: </p>
+<p>Anul absolvirii: </p>
+<p>Actul in baza caruia a fost admis: </p>
+<p>Media examenelor de absolvire: </p>
+<p>Media generala a notelor de absolvire: </p>
+<p>Limba de instruire: </p>
+<p>Limba straina studiata: </p>
+<p>Evidenta militara: </p>
+<p>Telefon fix: </p>
+<p>E-mail: </p>
+<p>Necesitatea cazarii in camin: </p>
+<p>Data complectarii: </p>
+<p>Responsabil: </p>
 @endforeach
 </body>
 </html>

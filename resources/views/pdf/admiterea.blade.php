f4bb4d4f88ed2cf7a2199ff8e4b2f6bc988643ab
sania <san_zhuk@mail.ru>
2016-06-24 10:37:05 +0300

    data add

--- resources/views/pdf/admiterea.blade.php
+++ resources/views/pdf/admiterea.blade.php
@@ -409,7 +409,9 @@
                 {{ucfirst($den->obiect1)}}
             </td>
             <td class="center">
+                @if($den->nota1 != 0)
                 {{$den->nota1}}
+                    @endif
             </td>
         </tr>
         <tr>
@@ -417,7 +419,9 @@
                 {{ucfirst($den->obiect2)}}
             </td>
             <td class="center">
+                @if($den->nota2 != 0)
                 {{$den->nota2}}
+                    @endif
             </td>
         </tr>
         <tr>
@@ -425,7 +429,9 @@
                 {{ucfirst($den->obiect3)}}
             </td>
             <td class="center">
+                @if($den->nota3 != 0)
                 {{$den->nota3}}
+                    @endif
             </td>
         </tr>
         <tr >
@@ -433,7 +439,9 @@
                 {{ucfirst($den->obiect4)}}
             </td>
             <td class="center">
+                @if($den->nota4 != 0)
                 {{$den->nota4}}
+                    @endif
             </td>
         </tr>
         <tr>
